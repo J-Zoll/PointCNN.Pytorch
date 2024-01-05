1655cca146fb8b8363868756b3a3c1da50f10281
J-Zoll <jonaszoll@gmx.de>
2024-01-05 17:50:32 +0100

    Refactor model to seperate file

---
+++ classifier.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+import torch
+from utils.model import RandPointCNN
+from utils.util_funcs import knn_indices_func_gpu, knn_indices_func_cpu
+from utils.util_layers import Dense
+
+
+# C_in, C_out, D, N_neighbors, dilution, N_rep, r_indices_func, C_lifted = None, mlp_width = 2
+# (a, b, c, d, e) == (C_in, C_out, N_neighbors, dilution, N_rep)
+# Abbreviated PointCNN constructor.
+AbbPointCNN = lambda a, b, c, d, e: RandPointCNN(a, b, 3, c, d, e, knn_indices_func_gpu)
+
+
+
+class Classifier(nn.Module):
+
+    def __init__(self, num_classes):
+        super(Classifier, self).__init__()
+
+        self.pcnn1 = AbbPointCNN(3, 32, 8, 1, -1)
+        self.pcnn2 = nn.Sequential(
+            AbbPointCNN(32, 64, 8, 2, -1),
+            AbbPointCNN(64, 96, 8, 4, -1),
+            AbbPointCNN(96, 128, 12, 4, 120),
+            AbbPointCNN(128, 160, 12, 6, 120)
+        )
+
+        self.fcn = nn.Sequential(
+            Dense(160, 128),
+            Dense(128, 64, drop_rate=0.5),
+            Dense(64, num_classes, with_bn=False, activation=None)
+        )
+
+    def forward(self, x):
+        x = self.pcnn1(x)
+        if False:
+            print("Making graph...")
+            k = make_dot(x[1])
+
+            print("Viewing...")
+            k.view()
+            print("DONE")
+
+            assert False
+        x = self.pcnn2(x)[1]  # grab features
+
+        logits = self.fcn(x)
+        logits_mean = torch.mean(logits, dim=1)
+        return logits_mean
